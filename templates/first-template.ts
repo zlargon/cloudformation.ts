5feb88c7ba14158da9b3b8d4f00737ceed964f9b
Leon Huang <zlargon1988@gmail.com>
2023-01-15 10:28:36 -0500

    ♻️  refactor first-template

--- templates/first-template.ts
+++ templates/first-template.ts
@@ -1,17 +1,9 @@
 #!/usr/bin/env -S deno run
 import { Stack } from '../src/Stack.ts';
 import { Tag } from '../src/Tag.ts';
-import { Ref } from '../src/Ref.ts';
 import { Fn_Sub } from '../src/Fn_Sub.ts';
 import { PseudoParameter } from '../src/PseudoParameter.ts';
 import { Fn_Format } from '../src/Fn_Format.ts';
-
-const Constant = {
-  InstanceType: 'InstanceType',
-  WebServerInstance: 'WebServerInstance',
-  WebSecurityGroup: 'WebServerSecurityGroup',
-  SQSQueue: 'SQSQueue',
-};
 
 // create stack
 const stack = Stack();
@@ -20,7 +12,7 @@ stack.setDescription('Our first template in CloudFormation course.');
 // ========================================================
 // Parameters
 // ========================================================
-stack.addParameter(Constant.InstanceType, {
+const InstanceType = stack.addParameter('InstanceType', {
   Type: 'String',
   Description: 'EC2 instance type',
   Default: 't2.micro',
@@ -29,21 +21,41 @@ stack.addParameter(Constant.InstanceType, {
 // ========================================================
 // Resources
 // ========================================================
+
+// Security Group
+const WebServerSecurityGroup = stack.addResource('WebServerSecurityGroup', {
+  Type: 'AWS::EC2::SecurityGroup',
+  Properties: {
+    GroupDescription: 'Security group for web servers',
+    SecurityGroupIngress: [
+      {
+        IpProtocol: 'tcp', //
+        FromPort: 80,
+        ToPort: 80,
+        CidrIp: '0.0.0.0/0',
+      },
+    ],
+    Tags: [
+      Tag('Name', 'Web server security group'), //
+    ],
+  },
+});
+
 // EC2 Instance
-stack.addResource(Constant.WebServerInstance, {
+const WebServerInstance = stack.addResource('WebServerInstance', {
   Type: 'AWS::EC2::Instance',
   Properties: {
     ImageId: 'ami-0b5eea76982371e91',
-    InstanceType: Ref(Constant.InstanceType),
+    InstanceType: InstanceType.Ref(),
     SecurityGroupIds: [
-      Ref(Constant.WebSecurityGroup), //
+      WebServerSecurityGroup.Ref(), //
     ],
     Tags: [
       Tag('Name', Fn_Sub('${AWS::StackName}-web-server')),
       Tag(
         'Name2',
         Fn_Sub('${AWS::StackName}-web-server-${instanceType}-${str}-${num}', {
-          instanceType: Ref(Constant.InstanceType),
+          instanceType: InstanceType.Ref(),
           str: 'hello',
           num: 123,
         })
@@ -52,7 +64,7 @@ stack.addResource(Constant.WebServerInstance, {
         'Name3',
         Fn_Format('{{stackName}}-web-server-{{instanceType}}-{{str}}-{{num}}', {
           stackName: PseudoParameter.StackName,
-          instanceType: Ref(Constant.InstanceType),
+          instanceType: InstanceType.Ref(),
           str: 'hello',
           num: 123,
         })
@@ -76,29 +88,10 @@ stack.addResource(Constant.WebServerInstance, {
   },
 });
 
-// Security Group
-stack.addResource(Constant.WebSecurityGroup, {
-  Type: 'AWS::EC2::SecurityGroup',
-  Properties: {
-    GroupDescription: 'Security group for web servers',
-    SecurityGroupIngress: [
-      {
-        IpProtocol: 'tcp', //
-        FromPort: 80,
-        ToPort: 80,
-        CidrIp: '0.0.0.0/0',
-      },
-    ],
-    Tags: [
-      Tag('Name', 'Web server security group'), //
-    ],
-  },
-});
-
 // SQS Queue
-stack.addResource(Constant.SQSQueue, {
+stack.addResource('SQSQueue', {
   Type: 'AWS::SQS::Queue',
-  DependsOn: Constant.WebServerInstance,
+  DependsOn: WebServerInstance.Name(),
 });
 
 console.log(stack.json());
